f214cc4edddcd3a0f9bac7984bc6b7772d59ae4f
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-08-08 02:52:57 +0200

    api: create basic CollectionsController structure

--- api/lib/mtg_tracker/controllers/CollectionsController.rb
+++ api/lib/mtg_tracker/controllers/CollectionsController.rb
@@ -3,12 +3,39 @@
 require 'sinatra/base'
 
 require_relative 'BaseController'
+require_relative '../models/Collection'
 
 module MtgTracker
   class CollectionsController < BaseController
 
     get '/' do
-      reply_with_message 'Collections'
+      items = MtgTracker::Collection.all
+      reply_with({ collections: items })
+    end
+
+    post '/' do
+      reply_with_error 501
+    end
+
+    get '/:id' do
+      unless params[:id].numeric?
+        reply_with_error 422, 'Id should be numeric'
+      end
+      
+      begin
+        item = MtgTracker::Collection.find params[:id]
+        reply_with({ collection: item })
+      rescue ActiveRecord::RecordNotFound
+        reply_with_error 404, "Can't find Collection with id #{params[:id]}"
+      end
+    end
+
+    put '/:id' do
+      reply_with_error 501
+    end
+
+    delete '/:id' do
+      reply_with_error 501
     end
   end
 end
