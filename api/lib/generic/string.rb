95f8915cc6a435347d0886deca715dd61d49e42c
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-08-08 02:51:13 +0200

    api: generic lib: add numeric? to String

--- api/lib/generic/string.rb
+++ api/lib/generic/string.rb
@@ -1,10 +1,16 @@
 
 class String
+  # convert from CamelCase to snake_case
   def underscore
     self.gsub(/::/, '/').
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
     gsub(/([a-z\d])([A-Z])/,'\1_\2').
     tr("-", "_").
     downcase
+  end
+
+  # check if a string is numeric
+  def numeric?
+    Float(self) != nil rescue false
   end
 end
