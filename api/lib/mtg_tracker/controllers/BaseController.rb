a2aed388c90b01915d5ec3d4d344b3e49ecb28f9
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-08-09 03:14:55 +0200

    api: clean BaseController

--- api/lib/mtg_tracker/controllers/BaseController.rb
+++ api/lib/mtg_tracker/controllers/BaseController.rb
@@ -2,14 +2,9 @@
 # require 'sinatra/base'
 
 require_relative '../application'
-require_relative '../sinatra/reply'
-require_relative '../sinatra/request'
 
 module MtgTracker
   class BaseController < Application
-
-    helpers Sinatra::ReplyHelpers
-    helpers Sinatra::RequestHelpers
 
   end
 end
