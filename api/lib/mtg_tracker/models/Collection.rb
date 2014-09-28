6da92c85875a37701ee8e22172680a4bb67c6a84
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-09-28 17:34:19 +0200

    api(Collection): enable serializable_hash

--- api/lib/mtg_tracker/models/Collection.rb
+++ api/lib/mtg_tracker/models/Collection.rb
@@ -11,19 +11,20 @@ module MtgTracker
       # This adds a .quantity property to the card objects
       lambda { select('cards.*, cards.id as "card_id", count(card_id) as "quantity"').group(:card_id) },
       through: :collectionables
-      
 
     validates :name,
       presence: true,
       uniqueness: true
 
-    # def serializable_hash(options = {})
-    #   options = options.try(:clone) || {}
-    #   # This will lead to A LOT of data...
-    #   # a cool idea could be to limit the query and add pagination
-    #   options[:include] = :cards
+    def serializable_hash(options = {})
+      options = options.try(:clone) || {}
+      
+      options[:except] = [ :created_at, :updated_at ]
+      # This will lead to A LOT of data...
+      # a cool idea could be to limit the query and add pagination
+      # options[:include] = :cards
 
-    #   super(options)
-    # end
+      super(options)
+    end
   end
 end
