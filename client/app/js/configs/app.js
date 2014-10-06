be88d7dfcb95485f2a58018ef07663fc7e46d8de
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-10-06 23:13:54 +0200

    client(mtgTracker): remove httpInterceptor config

--- client/app/js/configs/app.js
+++ client/app/js/configs/app.js
@@ -11,33 +11,8 @@
 
   // ConfigFunction. = [  ];
 
-  function ConfigFunction ($httpProvider) {
+  function ConfigFunction () {
 
-    // intercept HTTP errors
-    $httpProvider.interceptors.push(function($q) {
-      return {
-        'request': function(config) {
-          // console.debug('request', config)
-          return config;
-        },
-
-        'requestError': function(rejection) {
-          // console.debug('requestError', rejection)
-          return $q.reject(rejection);
-        },
-
-        'response': function(response) {
-          // console.debug('response', response)
-          return response;
-        },
-
-        'responseError': function(rejection) {
-          // console.debug('responseError', rejection)
-          alert('An error occurred trying to reach ' + rejection.config.url + ':\n'+rejection.message);
-          return $q.reject(rejection);
-        }
-      };
-    });
 
   };
 }());
