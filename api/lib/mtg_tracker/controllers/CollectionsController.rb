0f640517b43c3e67904cc99c849fa6d2b7f8a976
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-10-06 23:48:46 +0200

    api(CollectionsController): reply with current user collections

--- api/lib/mtg_tracker/controllers/CollectionsController.rb
+++ api/lib/mtg_tracker/controllers/CollectionsController.rb
@@ -12,13 +12,13 @@ module MtgTracker
 
     # Retrieve all collections
     get '/' do
-      items = Collection.all
+      items = @current_user.collections.all
       respond_with items
     end
 
     # Create a new collection
     post '/' do
-      collection = Collection.create!({ name: payload[:name] })
+      collection = @current_user.collections.create!({ name: payload[:name] })
       respond_with collection
     end
 
@@ -28,49 +28,49 @@ module MtgTracker
         halt_with_400 'Id should be numeric'
       end
       
-      item = Collection.find params[:id]
+      item = @current_user.collections.find params[:id]
       respond_with item
     end
 
     # Update specific collection
     put '/:id' do
-      collection = Collection.find(params[:id])
-      collection = Collection.update(collection.id, payload)
+      collection = @current_user.collections.find(params[:id])
+      collection = @current_user.collections.update(collection.id, payload)
       respond_with collection
 
     end
 
     # Delete specific collection
     delete '/:id' do
-      collection = Collection.find(params[:id])
+      collection = @current_user.collections.find(params[:id])
       collection.destroy
       respond_with collection
     end
 
     # Retrieve collection cards
     get '/:id/cards' do
-      collection = Collection.find(params[:id])
+      collection = @current_user.collections.find(params[:id])
       respond_with collection.cards
     end
 
     # Add a card to the collection
     post '/:id/cards' do
       card = Card.find(payload[:id])
-      Collection.find(params[:id]).cards.push(card)
+      @current_user.collections.find(params[:id]).cards.push(card)
 
       # we need to refetch the collection
-      collection = Collection.find(params[:id])
+      collection = @current_user.collections.find(params[:id])
       respond_with collection.cards
     end
 
     # Delete a card from the collection
     delete '/:id/cards' do 
       card = Card.find(payload[:id])
-      Collection.find(params[:id]).cards.delete(card)
+      @current_user.collections.find(params[:id]).cards.delete(card)
 
       # we need to refetch the collection
-      collection = Collection.find(params[:id])
-      respond_with cards: collection.cards
+      collection = @current_user.collections.find(params[:id])
+      respond_with collection.cards
     end
 
   end
