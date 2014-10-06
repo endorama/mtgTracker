5f2de88571e0bceb1319b86e3790cb34bc1c5e4e
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-10-06 23:12:02 +0200

    client(mtgTracker.collections): use iife for module definition

--- client/app/modules/collections/collections.module.js
+++ client/app/modules/collections/collections.module.js
@@ -1,3 +1,14 @@
-angular.module('mtgTracker.collections', [
-  'mtgTracker.api'
-]);
+(function () {
+  'use strict';
+  
+  /**
+    mtgTracker.collections
+
+    Handles cards collections
+   */
+
+  angular
+    .module('mtgTracker.collections', [
+      'mtgTracker.api'
+    ]);
+}());
