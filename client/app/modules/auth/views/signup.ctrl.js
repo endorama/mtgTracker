97bdcff54c997ad9ad4d18c69c28bfd491b42fb6
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-10-08 01:48:08 +0200

    client(mtgTracker.auth): add signup

--- client/app/modules/auth/views/signup.ctrl.js
+++ client/app/modules/auth/views/signup.ctrl.js
@@ -9,16 +9,35 @@
   // SignupCtrl.$inject = [  ];
 
   /* @ngInject */
-  function SignupCtrl ($auth) {
+  function SignupCtrl ($scope, AuthSrv) {
     var vm = this;
-    
 
     vm.signup = signup;
+    vm.form_has_error_on = form_has_error_on;
+    vm.has_signup_errors = has_signup_errors;
+    vm.signup_errors = signup_errors;
 
     //////////
+    var _signup_errors = null;
 
     function signup (formData) {
-      $auth.signup(formData);
+      AuthSrv.signup(formData)
+        .catch(function(reason) {
+          _signup_errors = reason.data.message;
+        });
+    }
+
+    function form_has_error_on (field) {
+      var field = $scope.SignupForm[field];
+      return !field.$pristine && field.$invalid;
+    };
+
+    function has_signup_errors () {
+      return !!_signup_errors;
+    }
+
+    function signup_errors () {
+      return _signup_errors;
     }
   };
 
