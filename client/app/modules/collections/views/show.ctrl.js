5544a9129ba4092e37afc35d90bda0ed183998bc
Edoardo Tenani <edoardo.tenani@gmail.com>
2014-11-06 22:48:31 +0100

    client(collections): add card and remove card from show view

--- client/app/modules/collections/views/show.ctrl.js
+++ client/app/modules/collections/views/show.ctrl.js
@@ -8,18 +8,42 @@
 
   // CollectionsShowCtrl.$inject = [  ];
 
-  function CollectionsShowCtrl(collection, cards) {
+  function CollectionsShowCtrl(collection, cards, $log, $flash, $state) {
     var vm = this;
 
+    vm.addCard    = addCard;
     vm.collection = collection;
-    vm.cards = cards;
+    vm.cards      = cards;
+    vm.removeCard = removeCard;
 
-    vm.rarityDropdown = [
-      { label: 'Mythic', value: 'Mythic'},
-      { label: 'Rare', value: 'Rare'},
-      { label: 'Uncommon', value: 'Uncommon'},
-      { label: 'Common', value: 'Common'},
-    ];
+    //////////
+
+    function addCard(card) {
+      collection.post('cards', card)
+        .then(function () {
+          $flash.s('The card has been added!');
+          $state.forceReload();
+        })
+        .catch(function (reason) {
+          $log.error(reason);
+          alert('Cannot add card to collection.');
+        });
+    }
+
+    function removeCard(card) {
+      console.debug(collection)
+      // collection.remove('cards', card)
+      collection.customDELETE('cards', card)
+        .then(function () {
+          $flash.s('The card has been removed!');
+          $state.forceReload();
+        })
+        .catch(function (reason) {
+          $log.error(reason);
+          alert('Cannot remove card from collection.');
+        });
+    }
+
   };
 
 }());
